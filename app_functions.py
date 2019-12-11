672a6f199b39e4724a7f3b94c74693f9b90500a5
Sohit Miglani <sohitmiglani@gmail.com>
2019-12-11 16:14:55 +0000

    completed first functional app

--- app_functions.py
+++ app_functions.py
@@ -1,48 +1,35 @@
-# This .py file houses all the major functions you'll use in your flask app. 
+# This .py file houses all the major functions you'll use in your flask app.
 # Have all the functions here and then import them in the flask_app.py file.
 
 import pandas # import all the required libraries here
 import random
-from flask import session
 
 __version__ = "1.0.1" # specify a version here in the backend
 
 def random_id(length): # This function has two doctests inside it to give an example on how to make doctests
     """
     Creates a random configuration key for the session - for safety of session variables.
-    
+
     >>> len(random_id(50)) == 50
     True
-    
-    
-    
+
     >>> random_id('hello')
     Traceback (most recent call last):
         ...
     TypeError: The input must be a positive integer.
-    
+
     """
     if type(length) != int or length < 1:
         raise TypeError('The input must be a positive integer.')
-    
+
     choices = '0123456789abcdefghijklmnopqrstuvwxyz'
-    
+
     id = ''
     for _ in range(length):
         id += random.choice(choices)
     return id
 
 
-def reset_session():
-    """
-    Resets all important session variables that are constantly parsed into the HTML template.
-    """
-    session.clear()
-    session['status'] = 'not_connected'
-    session['login_data'] = ''
-    session['output_data'] = ''
-
-    
 def log_me_in(userid, password):
     # Do something with userid and password
     data = 'You are logged in'
--- flask_app.py
+++ flask_app.py
@@ -1,99 +1,179 @@
 # importing general extensions of flask here
-from flask import Flask, render_template, request, session, redirect, flash, url_for, make_response, send_file
+from flask import Flask, session, render_template, request, flash, url_for, redirect, send_file, Response, make_response
+from io import BytesIO, StringIO
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.backends.backend_svg import FigureCanvasSVG
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 import flask
 from flask_bootstrap import Bootstrap
 import app_functions # importing the app_functions.py file to have all the functions ready
 from app_functions import __version__
-from flask_session import Session
-
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+import seaborn as sns
+plt.style.use('ggplot')
+plt.switch_backend('Agg')
 
 # The code for setting up a user session in flask and securing it with a secret_key is already installed below.
 # You can jump directly to building your functions, and collecting HTML inputs for processing.
 
-
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['DEBUG'] = True
-app.config['secret_key'] = app_functions.random_id(50)
-app.config['SESSION_TYPE'] = 'filesystem'
-sess = Session()
-sess.init_app(app)
+app.config["SECRET_KEY"] = app_functions.random_id(50)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 Bootstrap(app)
-
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+
     if 'status' not in session:
-        # Reset the sessin for you.
-        app_functions.reset_session()
+        session['input_data'] = ''
 
     if request.method == "POST":
-        if request.form["action"] == "reset":
-            # IF the reset button is hit, whole session is reset
-            app_functions.reset_session()
+        if request.form['submit_button'] == 'submit_data':
+            session['specimen'] = request.form["specimen"]
+            session['workflow'] = request.form["workflow"]
+            session['number_of_samples'] = request.form['number_of_samples']
+            input_file = request.files["metadata"]
+            session['input_data'] = input_file.stream.read().decode("utf-8")
 
-        elif request.form["action"] == "action_1":
+            input_annotation = request.files["annotation"]
+            session['annotation'] = input_annotation.stream.read().decode("utf-8")
 
-            # The value of a button in the HTML page must equal 'action_1' for this if loop to be triggered
+            if session['workflow'] == 'complete':
+                flash('The workflow for the complete analysis is not available yet. Only QC and visualisations are available.')
+            else:
+                return redirect(url_for('results'))
 
-            file = request.files["file"].read().decode("utf-8")
+    return render_template('home.html',
+                           version=__version__)
 
-            # This is how you collect form reponses. 
-            # The 'value' attribute of the inputs in HTML must match the string you put in the request.form/
-            # So the 'value' attribute of the user id form element must be 'user_id' so that the code below can recognise and receive the input.
-            user_id = request.form["user_id"]
-            password = request.form['password']
-            session['login_data'] = app_functions.log_me_in(user_id,password)
-            session['output_data'] = app_functions.process_file(file)
+@app.route("/scatter.png", methods=["GET", "POST"])
+def scatter():
+    dataframe = pd.read_csv(StringIO(session['input_data']))
 
-            #Changing the status variable so that the home page gets updated
-            session['status'] = 'connected'
+    sns.set()
 
-        elif request.form["action"] == "action_2":
-            # The value of a button in the HTML page must equal 'action_2' for this if loop to be triggered
-            # Do something here with the data you collect
-            flash('something happened here')
+    fig2, ax2 = plt.subplots()
 
-            # These messages will be flashed in the home.html page. 
-            # Look at home.html to see where the messages are flashed in the page.
-    
-        elif request.form["action"] == "action_3":
-            flash('This happened.')
+    for i in range(int(session['number_of_samples'])):
+        plt.scatter( list(dataframe['Sample'+str(i+1)]) ,[i+1 for _ in range(len(dataframe))] )
 
-            # Do something else here
-    
-    # Rendering the final template here.
-    # We use session variables to store any information and export it back to the HTML page
-    # In HTML, you can use these variables by adding the tag {{variable}}
-    # For example, if you want to output login_data, just use {{login_data}} in the HTML page
+    plt.ylabel('Samples')
+    plt.xlabel('Gene Count Distributions')
+    plt.title('Scatter Plot of Gene Counts for comparative analysis', fontsize= 15)
 
-    return render_template('home.html', 
-                           status =session['status'],
-                           version=__version__,
-                           login_data=session['login_data'],
-                           output_data=session['output_data'])
+    canvas = FigureCanvas(fig2)
+    img2 = BytesIO()
+    fig2.savefig(img2)
+    img2.seek(0)
 
+    return send_file(img2, mimetype='image/png')
 
-# This is another page within the same app that is triggered when you go to the server_url/bug/
-@app.route("/bug", methods=["GET", "POST"])
-def bug():
-    if request.method == "POST":
-        if request.form["action"] == "bug_action_1":
-            return redirect(url_for("home"))
-        elif request.form["action"] == "submit_report":
-            flash('something happened here')
-            # Do something here with maybe form elements or data you collect in the HTML page
+@app.route("/results", methods=["GET", "POST"])
+def results():
 
-    return render_template('bug_report.html', version=__version__)
+    dataframe = pd.read_csv(StringIO(session['input_data']))
+    analysis_data = []
 
+    import os
+    cwd = os.getcwd()
 
-# If there is a page that doesn't exist (a 404 error), it redirects you to the home page
+    for i in range(int(session['number_of_samples'])):
+        analysis_data.append(list(dataframe['Sample'+str(i+1)]))
+
+    model = PCA(n_components=2)
+    output = model.fit_transform(analysis_data)
+
+    sns.set()
+    plt.figure()
+    number_of_samples = int(session['number_of_samples'])
+    for i in range(number_of_samples):
+        plt.plot(output[i][0],output[i][1], marker='o', label='Sample'+str(i+1))
+
+    plt.ylabel('Second Component of PCA')
+    plt.xlabel('First Component of PCA')
+    plt.title('Quality Check: Clustering of the samples in PCA', fontsize= 15)
+    plt.legend()
+    plt.savefig(cwd + '/static/images/ml_plot.png')
+
+    plt.figure()
+    for i in range(number_of_samples):
+        plt.scatter( list(dataframe['Sample'+str(i+1)]) ,[i+1 for _ in range(len(dataframe))] )
+    plt.ylabel('Samples')
+    plt.xlabel('Gene Count Distributions')
+    plt.yticks(np.arange(number_of_samples+1),range(number_of_samples+1))
+    plt.title('Scatter Plot of Gene Counts for comparative analysis', fontsize= 15)
+    plt.savefig(cwd + '/static/images/scatter.png')
+
+    plt.figure()
+    plt.boxplot(analysis_data)
+    plt.ylabel('Transcript Counts for Log Transformed Gene Counts')
+    plt.xlabel('Samples')
+    plt.xticks(np.arange(number_of_samples+1),range(number_of_samples))
+    plt.title('Box Plots of Gene Counts for comparative analysis', fontsize= 15)
+    plt.savefig(cwd + '/static/images/box_plot.png')
+
+    medians1 = []
+    medians2 = []
+
+    condition1 = []
+    condition2 = []
+
+    annotation = pd.read_csv(StringIO(session['annotation']))
+
+    for i in range(number_of_samples):
+        if annotation['Condition'][i] == 1:
+            condition1.append(annotation['Sample'][i])
+        elif annotation['Condition'][i] == 2:
+            condition2.append(annotation['Sample'][i])
+
+    import statistics
+
+    for i in range(len(dataframe)):
+        first = []
+        second = []
+
+        for sample in condition1:
+            first.append(dataframe[sample][i])
+        for sample in condition2:
+            second.append(dataframe[sample][i])
+
+        medians1.append(statistics.median(first))
+        medians2.append(statistics.median(second))
+
+    highest = max(medians1 + medians2)
+
+    plt.figure()
+    plt.scatter(medians1,medians2)
+    plt.plot([0,highest],[0,highest],color='red',label='Normal expression')
+    plt.ylabel('Median of Samples for Condition 2')
+    plt.xlabel('Median of Samples for Condition 1')
+    plt.title('Scatter plot of median gene counts for Condition 1 Vs. Condition 2', fontsize= 15)
+    plt.legend()
+    plt.savefig(cwd + '/static/images/median_plot.png')
+
+    return render_template('results.html',
+                           version=__version__)
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 @app.errorhandler(404)
 def page_not_found(e):
     # the flash utlity flashes a message that can be shown on the main HTML page
     flash('The URL you entered does not exist. You have been redirected to the home page')
     return redirect(url_for('home'))
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
