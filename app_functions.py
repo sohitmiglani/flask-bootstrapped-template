23eba5dda05d89d4dd8e14c7603434a21c21c1e7
Sohit Miglani <sohitmiglani@gmail.com>
2020-01-24 19:44:36 -0800

    completed application

--- app_functions.py
+++ app_functions.py
@@ -3,6 +3,14 @@
 
 import pandas # import all the required libraries here
 import random
+import os
+import statistics
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+
 
 __version__ = "1.0.1" # specify a version here in the backend
 
@@ -30,11 +38,125 @@ def random_id(length): # This function has two doctests inside it to give an exa
     return id
 
 
-def log_me_in(userid, password):
-    # Do something with userid and password
-    data = 'You are logged in'
-    return data
+def make_all_visualisations(dataframe, annotation):
+    analysis_data = []
+    cwd = os.getcwd()
 
-def process_file(file):
-    # Take the file and do something with it
-    return 'This is the content of the file you uploaded'
+    number_of_samples = len(dataframe.columns) - 4
+    for i in range(number_of_samples):
+        analysis_data.append(list(dataframe['Sample'+str(i+1)]))
+
+    model = PCA(n_components=2)
+    output = model.fit_transform(analysis_data)
+
+    first_var = round(model.explained_variance_ratio_[0]*100,2)
+    second_var = round(model.explained_variance_ratio_[1]*100,2)
+
+    sns.set()
+    plt.figure()
+
+    for i in range(number_of_samples):
+        plt.plot(output[i][0],output[i][1], marker='o', label='Sample'+str(i+1))
+
+    plt.ylabel('Second Component of PCA')
+    plt.xlabel('First Component of PCA')
+    plt.title('Quality Check: Clustering of the samples in PCA', fontsize= 15)
+    plt.legend()
+    plt.savefig(cwd + '/static/images/ml_plot.png')
+
+    plt.figure()
+    for i in range(number_of_samples):
+        plt.scatter( list(dataframe['Sample'+str(i+1)]) ,[i+1 for _ in range(len(dataframe))] )
+    plt.ylabel('Samples')
+    plt.xlabel('Gene Count Distributions')
+    plt.yticks(np.arange(number_of_samples+1),range(number_of_samples+1))
+    plt.title('Scatter Plot of Gene Counts for comparative analysis', fontsize= 15)
+    plt.savefig(cwd + '/static/images/scatter.png')
+
+    plt.figure()
+    plt.boxplot(analysis_data)
+    plt.ylabel('Transcript Counts for Log Transformed Gene Counts')
+    plt.xlabel('Samples')
+    plt.xticks(np.arange(number_of_samples+1),range(number_of_samples+1))
+    plt.title('Box Plots of Gene Counts for comparative analysis', fontsize= 15)
+    plt.savefig(cwd + '/static/images/box_plot.png')
+
+    medians1 = []
+    medians2 = []
+
+    condition1 = []
+    condition2 = []
+
+    for i in range(number_of_samples):
+        if list(annotation['Condition'])[i] == 1:
+            condition1.append(annotation['Sample'][i])
+        elif list(annotation['Condition'])[i] == 2:
+            condition2.append(annotation['Sample'][i])
+
+    for i in range(len(dataframe)):
+        first = []
+        second = []
+
+        for sample in condition1:
+            first.append(dataframe[sample][i])
+        for sample in condition2:
+            second.append(dataframe[sample][i])
+
+        medians1.append(statistics.median(first))
+        medians2.append(statistics.median(second))
+
+    highest = max(medians1 + medians2)
+
+    plt.figure()
+    plt.scatter(medians1,medians2)
+    plt.plot([0,highest],[0,highest],color='red',label='Normal expression')
+    plt.ylabel('Median of Samples for Condition 2')
+    plt.xlabel('Median of Samples for Condition 1')
+    plt.title('Scatter plot of median gene counts for Condition 1 Vs. Condition 2', fontsize= 15)
+    plt.legend()
+    plt.savefig(cwd + '/static/images/median_plot.png')
+
+    plt.figure()
+    plt.scatter(dataframe['log2f'], dataframe['p_values'])
+    plt.ylabel('P-Values')
+    plt.xlabel('Log Fold Change')
+    plt.title('A Volcano plot of the p values against the log fold change.', fontsize= 15)
+    plt.axvline(0)
+    plt.savefig(cwd + '/static/images/volcano.png')
+
+    filtered_genes = []
+    filtered_names = []
+
+    for i in range(len(dataframe)):
+        if dataframe['p_values'][i] < 0.05:
+            if dataframe['log2f'][i] < -2 or dataframe['log2f'][i] > 2:
+                filtered_genes.append(i)
+                filtered_names.append(dataframe['gene'][i])
+
+    half_num = int(number_of_samples//2)
+    differences = []
+
+    for index in filtered_genes:
+        intermediate = []
+        for sample_num in range(1,half_num+1):
+            intermediate.append(dataframe['Sample' + str(sample_num + half_num)][index] - dataframe['Sample' + str(sample_num)][index])
+        differences.append(intermediate)
+
+    plt.figure()
+    for i in range(len(differences)):
+        plt.scatter(differences[i],[i+1 for _ in range(half_num)])
+    plt.ylabel('Genes')
+    plt.xlabel('Gene Count Differences (Condition 2 - Condition 1)')
+    plt.yticks(range(1,len(differences)+1),filtered_names)
+    plt.title('Gene Map of the differences between gene counts', fontsize= 15)
+    plt.savefig(cwd + '/static/images/gene_map.png')
+
+    new_dataframe = dataframe.iloc[filtered_genes,0:number_of_samples]
+    plt.figure()
+    sns.heatmap(new_dataframe)
+    plt.title('Heatmap of the gene counts for filtered genes')
+    plt.ylabel('Gene Counts')
+    plt.yticks(range(1,len(differences)+1),filtered_names)
+    plt.savefig(cwd + '/static/images/heatmap.png')
+
+    return first_var, second_var, len(filtered_genes)
--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,6 @@
 # importing general extensions of flask here
 from flask import Flask, session, render_template, request, flash, url_for, redirect, send_file, Response, make_response
 from io import BytesIO, StringIO
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.backends.backend_agg import FigureCanvasAgg
-from matplotlib.backends.backend_svg import FigureCanvasSVG
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 import flask
@@ -11,9 +8,7 @@ from flask_bootstrap import Bootstrap
 import app_functions # importing the app_functions.py file to have all the functions ready
 from app_functions import __version__
 import pandas as pd
-import numpy as np
-from sklearn.decomposition import PCA
-import seaborn as sns
+
 plt.style.use('ggplot')
 plt.switch_backend('Agg')
 
@@ -35,130 +30,34 @@ def home():
 
     if request.method == "POST":
         if request.form['submit_button'] == 'submit_data':
-            session['specimen'] = request.form["specimen"]
-            session['workflow'] = request.form["workflow"]
-            session['number_of_samples'] = request.form['number_of_samples']
             input_file = request.files["metadata"]
-            session['input_data'] = input_file.stream.read().decode("utf-8")
+            input_data = input_file.stream.read().decode("utf-8")
+            dataframe = pd.read_csv(StringIO(input_data))
 
             input_annotation = request.files["annotation"]
-            session['annotation'] = input_annotation.stream.read().decode("utf-8")
+            raw_annotation = input_annotation.stream.read().decode("utf-8")
+            annotation = pd.read_csv(StringIO(raw_annotation))
+            session['first_var'], session['second_var'], session['filtered_num'] = app_functions.make_all_visualisations(dataframe, annotation)
 
-            if session['workflow'] == 'complete':
-                flash('The workflow for the complete analysis is not available yet. Only QC and visualisations are available.')
-            else:
-                return redirect(url_for('results'))
+            return redirect(url_for('results'))
 
     return render_template('home.html',
                            version=__version__)
 
-@app.route("/scatter.png", methods=["GET", "POST"])
-def scatter():
-    dataframe = pd.read_csv(StringIO(session['input_data']))
-
-    sns.set()
-
-    fig2, ax2 = plt.subplots()
-
-    for i in range(int(session['number_of_samples'])):
-        plt.scatter( list(dataframe['Sample'+str(i+1)]) ,[i+1 for _ in range(len(dataframe))] )
-
-    plt.ylabel('Samples')
-    plt.xlabel('Gene Count Distributions')
-    plt.title('Scatter Plot of Gene Counts for comparative analysis', fontsize= 15)
-
-    canvas = FigureCanvas(fig2)
-    img2 = BytesIO()
-    fig2.savefig(img2)
-    img2.seek(0)
-
-    return send_file(img2, mimetype='image/png')
 
 @app.route("/results", methods=["GET", "POST"])
 def results():
-
-    dataframe = pd.read_csv(StringIO(session['input_data']))
-    analysis_data = []
-
-    import os
-    cwd = os.getcwd()
-
-    for i in range(int(session['number_of_samples'])):
-        analysis_data.append(list(dataframe['Sample'+str(i+1)]))
-
-    model = PCA(n_components=2)
-    output = model.fit_transform(analysis_data)
-
-    sns.set()
-    plt.figure()
-    number_of_samples = int(session['number_of_samples'])
-    for i in range(number_of_samples):
-        plt.plot(output[i][0],output[i][1], marker='o', label='Sample'+str(i+1))
-
-    plt.ylabel('Second Component of PCA')
-    plt.xlabel('First Component of PCA')
-    plt.title('Quality Check: Clustering of the samples in PCA', fontsize= 15)
-    plt.legend()
-    plt.savefig(cwd + '/static/images/ml_plot.png')
-
-    plt.figure()
-    for i in range(number_of_samples):
-        plt.scatter( list(dataframe['Sample'+str(i+1)]) ,[i+1 for _ in range(len(dataframe))] )
-    plt.ylabel('Samples')
-    plt.xlabel('Gene Count Distributions')
-    plt.yticks(np.arange(number_of_samples+1),range(number_of_samples+1))
-    plt.title('Scatter Plot of Gene Counts for comparative analysis', fontsize= 15)
-    plt.savefig(cwd + '/static/images/scatter.png')
-
-    plt.figure()
-    plt.boxplot(analysis_data)
-    plt.ylabel('Transcript Counts for Log Transformed Gene Counts')
-    plt.xlabel('Samples')
-    plt.xticks(np.arange(number_of_samples+1),range(number_of_samples))
-    plt.title('Box Plots of Gene Counts for comparative analysis', fontsize= 15)
-    plt.savefig(cwd + '/static/images/box_plot.png')
-
-    medians1 = []
-    medians2 = []
-
-    condition1 = []
-    condition2 = []
-
-    annotation = pd.read_csv(StringIO(session['annotation']))
-
-    for i in range(number_of_samples):
-        if annotation['Condition'][i] == 1:
-            condition1.append(annotation['Sample'][i])
-        elif annotation['Condition'][i] == 2:
-            condition2.append(annotation['Sample'][i])
-
-    import statistics
-
-    for i in range(len(dataframe)):
-        first = []
-        second = []
-
-        for sample in condition1:
-            first.append(dataframe[sample][i])
-        for sample in condition2:
-            second.append(dataframe[sample][i])
-
-        medians1.append(statistics.median(first))
-        medians2.append(statistics.median(second))
-
-    highest = max(medians1 + medians2)
-
-    plt.figure()
-    plt.scatter(medians1,medians2)
-    plt.plot([0,highest],[0,highest],color='red',label='Normal expression')
-    plt.ylabel('Median of Samples for Condition 2')
-    plt.xlabel('Median of Samples for Condition 1')
-    plt.title('Scatter plot of median gene counts for Condition 1 Vs. Condition 2', fontsize= 15)
-    plt.legend()
-    plt.savefig(cwd + '/static/images/median_plot.png')
-
     return render_template('results.html',
-                           version=__version__)
+                           version=__version__,
+                           first_var = session['first_var'],
+                           second_var = session['second_var'],
+                           filtered_num = session['filtered_num'])
+
+@app.route("/data", methods=["GET", "POST"])
+def data():
+    return render_template('data.html')
+
+
 
 @app.after_request
 def add_header(r):
